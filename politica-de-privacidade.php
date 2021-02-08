335c0b447f28faf9fb5427901c93241ade9f2cea
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2021-02-07 22:14:08 -0300

    Texto politica concluido

--- politica-de-privacidade.php
+++ politica-de-privacidade.php
@@ -3,6 +3,52 @@ include_once 'layouts/header.php';
 include_once 'layouts/menu.php';
 ?>
 
+<section id="inicio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-up">
+                <div>
+                    <h1>Política de Privacidade</h1>
+                    <h2>Nós da Facite Web Design, levamos muito a serio o cuidado e o sigilo dos dados de nossos clientes, confira nossa politica de privacidade.</h2>
+                </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
+                <img src="assets/img/inicio.png" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<main id="main">
+    <!-- ======= pol Section ======= -->
+    <section id="pol" class="pol">
+        <div class="container">
+
+            <div class="row" data-aos="zoom-in">
+                <div class="col-lg-9 text-center text-lg-left">
+                    <h2>Política de privacidade da <strong>Facite Web Design</strong></h2><br>
+                    <p>Aqui na Facite Web Design, você tem a garantia de que dados pessoais de nossos usuários estão em total confidencialidade, pois é muito importante para nosso site.</p><br>
+                    <p>Todas essas informações, serão usadas para lhe ajudar a ter uma experiência melhor e mais produtiva em nosso site</p><br>
+                    <p>Ressaltamos ainda que, essas informações pessoais, de usuários, clientes, funcionários e visitantes que usam nossa plataforma, serão tratados de acordo com o Lei Geral de Proteção de Dados (LGPD art. 17) onde "Toda pessoa natural tem assegurada a titularidade de seus dados pessoais e garantidos os direitos fundamentais de liberdade, de intimidade e de privacidade".</p><br>
+                    <p>O titular dos dados pessoais tem direito a obter do controlador, em relação aos dados do titular por ele tratados, a qualquer momento e mediante requisição:</p>
+                    <p>I - confirmação da existência de tratamento;</p>
+                    <p>II - acesso aos dados mantidos pelo controlador;</p>
+                    <p>III - correção de dados incompletos, inexatos ou desatualizados;</p>
+                    <p>IV - anonimização, bloqueio ou eliminação de dados desnecessários, excessivos ou tratados em desconformidade com o disposto na LGPD;</p>
+                    <p>V - portabilidade dos dados a outro fornecedor de serviço ou produto, mediante requisição expressa;</p>
+                    <p>VI - eliminação dos dados pessoais tratados quando revogado o consentimento dado pelo titular;</p>
+                    <p>VII - informação com quem o controlador realizou compartilhamento de seus dados;</p>
+                    <p>VIII - informação sobre a possibilidade de não fornecer consentimento e sobre as consequências da negativa;</p>
+                    <p>IX - revogação do consentimento.</p>
+                    <p style="font-size: small;">Para mais informações sobre a LGPD art. 17 <a href="https://www.gov.br/defesa/pt-br/acesso-a-informacao/lei-geral-de-protecao-de-dados-pessoais-lgpd">clique aqui</a></p><br><br>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End pol Section -->
+</main>
+
 <?php
 include_once 'layouts/footer.php';
 ?>
